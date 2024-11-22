9ce28f24a487f73ace69f7aa5a3c06b03fef17a7
0-4849 <tars.nijman@gmail.com>
2024-11-22 11:53:44 +0100

    added function to calculate the number of words which can be filled in in a certain row or column

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,15 @@
+use std::cmp::min_by;
 use std::fs::File;
 use std::io::prelude::*;
+use std::time::Instant;
 
-const GRID_WIDTH: usize = 4;
+#[derive(Debug)]
+enum Direction {
+    Horizontal,
+    Vertical,
+}
+
+const GRID_WIDTH: usize = 3;
 const GRID_HEIGHT: usize = 3;
 const MAX_WORD_LENGTH: usize = 25;
 
@@ -26,8 +34,21 @@ fn main() -> std::io::Result<()> {
     }
 
     let grid = [[0; GRID_WIDTH]; GRID_HEIGHT];
+
+    let start = Instant::now();
     let solution = solve(grid, &dictionary);
+    let elapsed = start.elapsed();
+
     let solution_string = solution.map(|s| format_solution(&s));
+
+    println!("took {:?}", elapsed);
+
+    let partial = [[98, 101, 100], [0, 0, 0], [0, 0, 0]];
+
+    println!(
+        "{:?}",
+        determine_most_constrained_variable(&partial, &dictionary)
+    );
 
     if let Some(s) = solution_string {
         println!("{}", s);
@@ -66,6 +87,94 @@ fn solve(
     }
 
     Some(grid)
+}
+
+fn determine_most_constrained_variable(
+    grid: &[[u8; GRID_WIDTH]; GRID_HEIGHT],
+    dictionary: &Vec<Vec<&[u8]>>,
+) -> Option<(Direction, usize)> {
+    // store the number of possible words which
+    // can be filled in for each of the columns
+    let mut number_of_options_rows = vec![0; grid.len()];
+    let mut number_of_options_cols = vec![0; grid[0].len()];
+
+    for (number_of_options, row) in number_of_options_rows.iter_mut().zip(grid) {
+        // we will binary search the (sorted) dictionary for
+        // the lower bound, above which are words which can fill in row
+        // the upper bound, below which are words which can fill in row
+        // we expect the search to fail, but for it to return
+        // the point where it has failed
+        // we can then take the difference of the lower and upper bound
+        // and this gives us the number of options which can fill in row
+        let lower_bound = row;
+        let mut upper_bound = row.clone();
+
+        for c in &mut upper_bound {
+            if *c == 0 {
+                *c = u8::MAX;
+            }
+        }
+
+        let lower_bound_pos = match dictionary[GRID_WIDTH].binary_search(&&lower_bound[..]) {
+            Ok(x) => x,
+            Err(x) => x,
+        };
+
+        let upper_bound_pos = match dictionary[GRID_WIDTH].binary_search(&&upper_bound[..]) {
+            Ok(x) => x,
+            Err(x) => x,
+        };
+
+        *number_of_options = upper_bound_pos - lower_bound_pos;
+    }
+
+    for (col_index, number_of_options) in number_of_options_cols.iter_mut().enumerate() {
+        let mut lower_bound = [0; GRID_HEIGHT];
+        for row_index in 0..grid.len() {
+            lower_bound[row_index] = grid[row_index][col_index];
+        }
+        let mut upper_bound = lower_bound.clone();
+
+        for c in &mut upper_bound {
+            if *c == 0 {
+                *c = u8::MAX;
+            }
+        }
+
+        let lower_bound_pos = match dictionary[GRID_WIDTH].binary_search(&&lower_bound[..]) {
+            Ok(x) => x,
+            Err(x) => x,
+        };
+
+        let upper_bound_pos = match dictionary[GRID_WIDTH].binary_search(&&upper_bound[..]) {
+            Ok(x) => x,
+            Err(x) => x,
+        };
+
+        *number_of_options = upper_bound_pos - lower_bound_pos;
+    }
+
+    let (min_row_index, min_row) = number_of_options_rows
+        .iter()
+        .enumerate()
+        .min_by(|(_, m), (_, n)| m.cmp(n))
+        .unwrap();
+
+    let (min_col_index, min_col) = number_of_options_cols
+        .iter()
+        .enumerate()
+        .min_by(|(_, m), (_, n)| m.cmp(n))
+        .unwrap();
+
+    println!("{:?}, {:?}", number_of_options_cols, number_of_options_rows);
+
+    if *min_row == 0 && *min_col == 0 {
+        None
+    } else if min_row > min_col {
+        Some((Direction::Horizontal, min_row_index))
+    } else {
+        Some((Direction::Vertical, min_col_index))
+    }
 }
 
 fn format_solution(&solution: &[[u8; GRID_WIDTH]; GRID_HEIGHT]) -> String {
