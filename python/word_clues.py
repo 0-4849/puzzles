e4666933fffe01eb6efa7898482e78179ac73552
0-4849 <110551701+0-4849@users.noreply.github.com>
2025-01-10 13:07:04 +0100

    Added comments to word_clues.py

--- python/word_clues.py
+++ python/word_clues.py
@@ -1,15 +1,21 @@
 import requests, random, time, subprocess
 from bs4 import BeautifulSoup
 
+# call the rust program for generating the puzzle, and store the output in solution
 solution = subprocess.run("../rust/target/release/word_puzzle", capture_output=True).stdout.decode("utf-8")
 print(solution)
 
+# all horizontal words are on one line, seperated by comma's
+# vertical words are on the next line
+# the *_ indicates that all other lines should be ignored
 [horizontal_words, vertical_words, *_] = [i.split(",") for i in solution.splitlines()]
 
 horizontal_clues = []
 vertical_clues = []
 
+# go through all the words and look them up on the puzzelwoordenboek
 for word in horizontal_words:
+    # request page for each word
     word_page = requests.get(f"https://www.mijnwoordenboek.nl/puzzelwoordenboek/{word}/1")
     # convert to soup
     soup = BeautifulSoup(word_page.text, 'html.parser')
@@ -23,14 +29,19 @@ for word in horizontal_words:
         for j in i.find_all("div") 
         if (s := j.string) is not None
     ]
+    # sanity check print statement
     print(candidates)
+    # sometimes, a word isn't found, so candidates will be empty  
+    # and random.choice will throw an error, so this check is here
     if candidates:
         horizontal_clues.append(random.choice(candidates))
     else:
         horizontal_clues.append("geen betekenis gevonden")
 
+    # wait between requests to ensure not getting banned
     time.sleep(3)
 
+# do the same thing for the vertical words
 for word in vertical_words:
     word_page = requests.get(f"https://www.mijnwoordenboek.nl/puzzelwoordenboek/{word}/1")
     soup = BeautifulSoup(word_page.text, 'html.parser')
@@ -46,7 +57,7 @@ for word in vertical_words:
         vertical_clues.append("geen betekenis gevonden")
     time.sleep(3)
 
-# implicit string concatination ftw
+# print out the clues in a nice format, along with the solution
 print("Horizontaal:\n" + "\n".join([f"{i + 1}: {word}" for (i,word) in enumerate(horizontal_clues)]))
 print("Verticaal:\n"   + "\n".join([f"{i + 1}: {word}" for (i,word) in enumerate(vertical_clues)]))
 print(f"Oplossing horizontaal: {horizontal_words}")
